59452383d3dd02fff74b6ecfbb58a8d02c44f6b7
Norman Syarif <norman.syf@gmail.com>
2018-02-28 17:39:24 +0700

    Fixing encryption method

--- app/src/main/java/id/ac/unja/si/ktmwriter/EncryptLibrary.java
+++ app/src/main/java/id/ac/unja/si/ktmwriter/EncryptLibrary.java
@@ -11,8 +11,8 @@ import javax.crypto.spec.SecretKeySpec;
  * Created by norman on 2/19/18.
  */
 
-public class EncryptLibrary {
-    public static String encrypt(String input, String key){
+class EncryptLibrary {
+    static String encrypt(String input, String key){
         byte[] crypted;
         try{
             SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
@@ -22,7 +22,16 @@ public class EncryptLibrary {
         }catch(Exception e){
             return e.toString();
         }
-        return new String(Base64.encodeBase64(crypted));
+        return clean(new String(Base64.encodeBase64(crypted)));
+    }
+
+    private static String clean(String str) {
+        String from = "+/";
+        String to = "-_";
+        for (int i = 0; i < from.length(); i++) {
+            str = str.replace(from.charAt(i), to.charAt(i));
+        }
+        return str.replace("=", "");
     }
 
 }
